72f679b91b06ca41e27f2d4efd7c680ce4db2968
Velvizhi Palanisamy <velvizhi.p@ptpll611.payoda.com>
2018-04-25 09:45:32 +0530

    "Add google maps"

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-about',
@@ -8,6 +9,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  location:string = "";
+  latitude:number = 0;
+  longitude:number = 0;
 
   constructor(private route: ActivatedRoute, private router: Router) { 
     this.route.params.subscribe(res => console.log(res.id));    
@@ -16,6 +22,25 @@ export class AboutComponent implements OnInit {
     this.router.navigate(['']);
   }
   ngOnInit() {
+    this.getLocation();
   }
-
+  getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          this.showPosition(position);
+        });
+    } else {
+      this.location = "Geolocation is not supported by this browser.";
+    }
+  }
+  showPosition(position) {
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    var mapProp = {
+      center: new google.maps.LatLng(this.latitude, this.longitude),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
